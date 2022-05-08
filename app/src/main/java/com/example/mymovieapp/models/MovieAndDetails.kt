a38d6465bd112af233301d82d644daa398ebb06c
milad parvaneh <parvanehmilad73@yahoo.com>
2022-05-08 12:35:25 +0430

    finish

---
+++ app/src/main/java/com/example/mymovieapp/models/MovieAndDetails.kt
@@ -0,0 +1,13 @@
+package com.example.mymovieapp.models
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+//data class MovieAndDetails(
+//    @Embedded val movie: Result,
+//    @Relation(
+//parentColumn = "id",
+//entityColumn = "movieOwnerId"
+//)
+//    val movieDetails: MovieDetails
+//)
